eca66c626e03235b8cb0b91a18e9db4ab6f49b57
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-02-18 19:05:45 -0300

    Nested components

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,16 +1,51 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+/* //Nested components
 function Greeting() {
   return (
     <div>
-      <h4>This is Alan and this is my first component!!!</h4>
+      <Person />
+      <Message />
     </div>
   );
 }
+const Person = () => <h2>Alan Jones</h2>;
+const Message = () => {
+  return <p>This is my message</p>;
+};
 
-// const Greeting = () => {
-//   return React.createElement("h1", {}, "Hello World");
-// };
+ReactDom.render(<Greeting />, document.getElementById("root")); */
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+function BookList() {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => {
+  return (
+    <img
+      src="https://http2.mlstatic.com/D_NQ_NP_722711-MLA42906730908_072020-O.webp"
+      alt="BOOK"
+    />
+  );
+};
+const Title = () => <h1>Harry Potter y la piedra filosofal</h1>;
+const Author = () => <h2>J. K. Rowling</h2>;
+
+ReactDom.render(<BookList />, document.getElementById("root"));

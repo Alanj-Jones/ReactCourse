db1b3d62bbf7968f2e8388a3805a5da4bca1ebd4
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-02-18 18:34:25 -0300

    Components & JSX Rules

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react";
+import ReactDom from "react-dom";
 
 function Greeting() {
-    return <h4>This is Alan and this is my first component!!!</h4>
+  return (
+    <div>
+      <h4>This is Alan and this is my first component!!!</h4>
+    </div>
+  );
 }
 
+// const Greeting = () => {
+//   return React.createElement("h1", {}, "Hello World");
+// };
 
-ReactDom.render(<Greeting/>, document.getElementById("root"));
+ReactDom.render(<Greeting />, document.getElementById("root"));

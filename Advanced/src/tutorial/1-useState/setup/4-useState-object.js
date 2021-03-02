322bd2979a663049f10cc4fe56099de8e1f259f9
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-03-01 21:23:06 -0300

    Use State Objects

--- Advanced/src/tutorial/1-useState/setup/4-useState-object.js
+++ Advanced/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Peter",
+    age: 24,
+    message: "Random Message",
+  });
+
+  const changeMessage = () => {
+    // setMessage("Hello From React!");
+    setPerson({ ...person, message: "Hello From React" });
+  };
+
+  /* Mi solucion:
+  const changeName = () => {
+    setPerson({ ...person, name: "Alan" });
+  };
+
+  const changeAge = () => {
+    setPerson({ ...person, age: 21 });
+  };
+  */
+
+  /* Para usar esto sacar 'person.' de el return
+  const [name, setName] = useState("Peter");
+  const [age, setAge] = useState("24");
+  const [message, setMessage] = useState("Random Message");
+  */
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      {/* <button className="btn" type="button" onClick={changeName}>
+        Change Name
+      </button>
+      <button className="btn" type="button" onClick={changeAge}>
+        Change Age
+      </button> */}
+      <button className="btn" type="button" onClick={changeMessage}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;

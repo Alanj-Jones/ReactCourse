4a6150cc190647f9333ee29a337fa75edc6c566e
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-03-02 20:43:21 -0300

    Functional update

--- Advanced/src/tutorial/1-useState/setup/5-useState-counter.js
+++ Advanced/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -3,6 +3,16 @@ import React, { useState } from "react";
 const UseStateCounter = () => {
   const [value, setValue] = useState(0);
 
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1);
+      //Functional approach
+      setValue((prevValue) => {
+        return prevValue + 1;
+      });
+    }, 2000);
+  };
+
   return (
     <React.Fragment>
       <section style={{ margin: "4rem 0" }}>
@@ -24,6 +34,13 @@ const UseStateCounter = () => {
           onClick={() => setValue(value + 1)}
         >
           Increase
+        </button>
+      </section>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>More COMPLEX counter</h2>
+        <h1>{value}</h1>
+        <button type="button" className="btn" onClick={complexIncrease}>
+          Increase later
         </button>
       </section>
     </React.Fragment>

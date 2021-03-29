5a89c56f727f7a9e40f5bea6f13e31fa92ce0271
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-03-29 20:08:16 -0300

    Ternary operator

--- Advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ Advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,12 +3,15 @@ import React, { useState } from "react";
 // ternary operator
 
 const ShortCircuit = () => {
-  const [text, setText] = useState("Test");
-  const firstValue = text || "hello world";
+  const [text, setText] = useState("");
+
+  const [isError, setIsError] = useState(false);
+
+  /*const firstValue = text || "hello world";*/
 
   //Si el primero es verdadero devuelve el segundo valor, pero si el primero
   // es falso devuelve el primero
-  const secondValue = text && "hello world";
+  /*const secondValue = text && "hello world";*/
 
   return (
     <>
@@ -18,10 +21,22 @@ const ShortCircuit = () => {
       {/* Si 'text' esta vacio/es falso devuelve la segunda opcion, si es verdadero 
       devuelve su valor */}
       <h1>{text || "A Text"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        Toggle Error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? <p>IsError : True</p> : <p>IsError : False</p>}
+      {isError ? (
+        <p>There is an error...</p>
+      ) : (
+        <div>
+          <h3>Ther is NO ERROR</h3>
+        </div>
+      )}
 
-      {/* Con el operador AND, si 'text' es verdadero devuelve el segundo valor */}
+      {/* Con el operador AND, si 'text' es verdadero devuelve el segundo valor
       {text && <h1>Hello world</h1>}
-      {!text && <h1>Hello world</h1>}
+      {!text && <h1>Hello world</h1>} */}
     </>
   );
 };

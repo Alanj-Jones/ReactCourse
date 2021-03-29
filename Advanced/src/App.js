a59c4870533bdfcae161e3b2f7d9d5b8ec527590
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-03-28 23:03:24 -0300

    Short circuit expressions

--- Advanced/src/App.js
+++ Advanced/src/App.js
@@ -6,12 +6,15 @@ import Setup from "./tutorial/1-useState/setup/3-useState-array";
 import Setup from "./tutorial/1-useState/setup/4-useState-object";
 import Setup from "./tutorial/1-useState/setup/5-useState-counter";
 */
-// import Setup from "./tutorial/2-useEffect/setup/1-useEffect-basics";
-// import Setup from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
-// import Setup from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
-import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+/* Use Effect
+import Setup from "./tutorial/2-useEffect/setup/1-useEffect-basics";
+import Setup from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
+import Setup from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
+*/
+
 // import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
-// import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+import Setup from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
+// import Setup from "./tutorial/3-conditional-rendering/setup/3-show-hide";
 
 function App() {
   return (
--- Advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ Advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("Test");
+  const firstValue = text || "hello world";
 
-  return <h2>short circuit</h2>;
+  //Si el primero es verdadero devuelve el segundo valor, pero si el primero
+  // es falso devuelve el primero
+  const secondValue = text && "hello world";
+
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>Value: {secondValue}</h1> */}
+
+      {/* Si 'text' esta vacio/es falso devuelve la segunda opcion, si es verdadero 
+      devuelve su valor */}
+      <h1>{text || "A Text"}</h1>
+
+      {/* Con el operador AND, si 'text' es verdadero devuelve el segundo valor */}
+      {text && <h1>Hello world</h1>}
+      {!text && <h1>Hello world</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;

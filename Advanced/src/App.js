ed3bb777ec34966d565538f37c11f9510256c7e2
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-04-01 13:55:58 -0300

    Forms: Multiple inputs

--- Advanced/src/App.js
+++ Advanced/src/App.js
@@ -16,8 +16,8 @@ import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
 import Setup from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
 import Setup from "./tutorial/3-conditional-rendering/setup/3-show-hide";
 */
-import Setup from "./tutorial/4-forms/setup/1-controlled-inputs";
-// import Setup from "./tutorial/4-forms/setup/2-multiple-inputs";
+// import Setup from "./tutorial/4-forms/setup/1-controlled-inputs";
+import Setup from "./tutorial/4-forms/setup/2-multiple-inputs";
 
 function App() {
   return (

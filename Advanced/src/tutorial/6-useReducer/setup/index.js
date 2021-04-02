78f85dc83f015d86040fbd4512317ca207a409a9
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-04-02 20:54:32 -0300

    Use Reduce : Add items

--- Advanced/src/tutorial/6-useReducer/setup/index.js
+++ Advanced/src/tutorial/6-useReducer/setup/index.js
@@ -2,25 +2,48 @@ import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import { data } from "../../../data";
 // reducer function
-
+const reducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const newPeople = [...state.people, action.payload];
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "Item Added",
+    };
+  }
+  if (action === "NO_VALUE") {
+    return {
+      ...state,
+      isModalOpen: true,
+      modalContent: "Enter a value !",
+    };
+  }
+  throw new Error("No matching action Type");
+};
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "Hello World!",
+};
 const Index = () => {
   const [name, setName] = useState("");
-  const [people, setPeople] = useState(data);
-  const [showModal, setShowModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-      setShowModal(true);
-      setPeople([...people, { id: new Date().getTime().toString, name }]);
+      const newPeople = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newPeople });
       setName("");
     } else {
-      setShowModal(true);
+      dispatch({ type: "NO_VALUE" });
     }
   };
 
   return (
     <>
-      {showModal && <Modal />}
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input
@@ -31,7 +54,7 @@ const Index = () => {
         </div>
         <button type="submit">Add</button>
       </form>
-      {people.map((person) => {
+      {state.people.map((person) => {
         return (
           <div key={person.id}>
             <h4>{person.name}</h4>

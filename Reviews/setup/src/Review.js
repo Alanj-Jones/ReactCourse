52927af75f972d3c5602af5aea59d9c567787a23
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-04-06 02:56:51 -0300

    Progress

--- Reviews/setup/src/Review.js
+++ Reviews/setup/src/Review.js
@@ -1,9 +1,25 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  // const { id, name, job, image, text } = people;
+  const [index, setIndex] = useState(0);
+  const { id, name, job, text, image } = people[index];
+
+  return (
+    <article className="container">
+      <img src={image} alt="Profile Picture" />
+      <h3></h3>
+      <h2></h2>
+      <p></p>
+      <div>
+        <button></button>
+        <button></button>
+      </div>
+      <button></button>
+    </article>
+  );
 };
 
 export default Review;

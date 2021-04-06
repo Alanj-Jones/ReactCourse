d3facfd2788432cc285bcb12ffdd32119a638e76
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-04-06 03:50:42 -0300

    Reviews finished

--- Reviews/setup/src/Review.js
+++ Reviews/setup/src/Review.js
@@ -3,21 +3,51 @@ import people from "./data";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  // const { id, name, job, image, text } = people;
   const [index, setIndex] = useState(0);
   const { id, name, job, text, image } = people[index];
 
+  const prevReview = () => {
+    if (index === 0) {
+      setIndex(people.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
   return (
-    <article className="container">
-      <img src={image} alt="Profile Picture" />
-      <h3></h3>
-      <h2></h2>
-      <p></p>
-      <div>
-        <button></button>
-        <button></button>
+    <article className="review">
+      <div className="container">
+        <div className="img-container">
+          <img src={image} alt="Profile Picture" className="person-img" />
+          <div className="quote-icon">
+            <h3>~</h3>
+          </div>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div>
+          <button className="prev-btn" onClick={() => prevReview()}>
+            {"<"}
+          </button>
+          {console.log(index)}
+          <button
+            className="next-btn"
+            onClick={() =>
+              setIndex(index === people.length - 1 ? 0 : index + 1)
+            }
+          >
+            {">"}
+          </button>
+        </div>
+        <button
+          className="random-btn"
+          onClick={() =>
+            setIndex(Math.floor(Math.random() * (people.length - 1)))
+          }
+        >
+          Random Review!
+        </button>
       </div>
-      <button></button>
     </article>
   );
 };

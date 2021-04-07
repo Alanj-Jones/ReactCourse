e43f9044695a6a04cf8cd915021f31c47ea2d1ad
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-04-07 16:59:59 -0300

    Accordion Finished

--- Accordion/setup/src/App.js
+++ Accordion/setup/src/App.js
@@ -1,8 +1,27 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  // const { id, info, title } = data;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>Preguntas y respuestas acerca del inicio de sesion</h3>
+        <div>
+          {questions.map((question) => {
+            return (
+              <SingleQuestion
+                key={question.id}
+                info={question.info}
+                title={question.title}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;

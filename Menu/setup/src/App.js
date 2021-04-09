5e2098617ce015495545a7e87d5f94d93fb91af2
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-04-09 19:05:35 -0300

    App.js finished

--- Menu/setup/src/App.js
+++ Menu/setup/src/App.js
@@ -1,10 +1,29 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [isVisible, setIsVisible] = useState(true);
+  const [index, setIndex] = useState(0);
+  const { id, title, category, price, img, desc } = items[index];
+  return (
+    <main>
+      <article className="menu">
+        <header className="title">
+          <h2>Nuestro menu</h2>
+          <div className="underline"></div>
+        </header>
+        <div className="btn-container">
+          <button className="filter-btn">Todo</button>
+          <button className="filter-btn">Desayuno</button>
+          <button className="filter-btn">Almuerzo</button>
+          <button className="filter-btn">Milkshakes</button>
+        </div>
+        <Menu key={id} />
+      </article>
+    </main>
+  );
 }
 
 export default App;
